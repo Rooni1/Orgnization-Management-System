c8c23a2e1729167cfe4f394b36c86e88b902f8ca
Haroon munir <haroon_munir@hotmail.com>
2025-10-08 12:53:14 +0200

    Added Update and delete function

--- EmployeeManagement/Controllers/ProjectController.cs
+++ EmployeeManagement/Controllers/ProjectController.cs
@@ -23,14 +23,15 @@ namespace EmployeeManagement.Controllers
         [HttpGet]
         public async Task<IActionResult> CreateProject()
         {
-            var departments = await _departmentBroker.GetDepartments();
-            ViewBag.Departments = new SelectList(departments,"Id","Name");
+            var department = await _departmentBroker.GetDepartments();
+            ViewBag.Departments = new SelectList(department, "Id", "Name");
 
             return View();
         }
         [HttpPost]
         public async Task<IActionResult> CreateProject(ProjectVM projectVM)
         {
+            
             if (ModelState.IsValid)
             {
                 // Call the broker to create the project
@@ -40,7 +41,7 @@ namespace EmployeeManagement.Controllers
             }
 
             var departments = await _departmentBroker.GetAllDepartments();
-            ViewBag.Departments = new SelectList(departments, "Id", "Name");
+            ViewBag.Departments = new SelectList(departments, "id", "Name");
             return View(projectVM);
         }
         [HttpGet]
@@ -49,6 +50,28 @@ namespace EmployeeManagement.Controllers
             var projects = await _projectBroker.GetProjects();
             return View(projects);
         }
+        [HttpGet]
+        public IActionResult UpdateProject(Guid id)
+        {
+            var peroject = _projectBroker.GetProjectById(id);
+            if (peroject != null)
+            {
+                return View(peroject);
+            }
 
+            return RedirectToAction("UpdateProject");
+        }
+        [HttpPost]
+        public IActionResult UpdateProject(ProjectVM projectVM)
+        {
+            _projectBroker.UpdateProject(projectVM);
+            return RedirectToAction("GetProjects");
+        }
+        [HttpPost]
+        public IActionResult DeleteProject(ProjectVM projectVM)
+        {
+            _projectBroker.DeleteProject(projectVM);
+            return RedirectToAction("GetProjects");
+        }
     }
 }
